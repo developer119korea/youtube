462fd86bd009cd3ba49749d39b414b3691914150
Youngwan Cho <developer119korea@gmail.com>
2021-12-14 18:21:18 +0900

    유튜브 서비스 객체 분리

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,23 @@
 import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
-import dotenv from "dotenv";
 import SearchHeader from './components/search_header/search_header';
-dotenv.config();
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
 
-  const search = (query) => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/search?part=snippet&maxResults=25&q=${query}&type=video&key=${process.env.REACT_APP_API_SERVICE_KEY}`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({ ...item, id: item.videoId })))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+  const search = query => {
+    youtube
+      .search(query)
+      .then(vidoes => setVideos(vidoes));
   }
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_API_SERVICE_KEY}`, requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()
+      .then(vidoes => setVideos(vidoes));
   }, []);
+
   return (
     <div className={styles.app} >
       <SearchHeader onSearch={search} />
---
+++ src/service/youtube.js
@@ -0,0 +1,26 @@
+class Youtube {
+  constructor(baseUrl, serviceKey) {
+    this.baseUrl = baseUrl;
+    this.serviceKey = serviceKey;
+    this.getRequestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+  }
+
+  async mostPopular() {
+    const response = await fetch(`${this.baseUrl}/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.serviceKey}`,
+      this.getRequestOptions);
+    const result = await response.json();
+    return result.items;
+  }
+
+  async search(query) {
+    const response = await fetch(`${this.baseUrl}/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.serviceKey}`,
+      this.getRequestOptions);
+    const result = await response.json();
+    return result.items.map(item => ({ ...item, id: item.videoId }));
+  }
+}
+
+export default Youtube;
