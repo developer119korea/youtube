34bae0a7f04bb68840bd974de45ae98b3213deca
Youngwan Cho <developer119korea@gmail.com>
2021-12-14 13:01:53 +0900

    인기 동영상 제목을 리스트로 보여준다

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
+import dotenv from "dotenv";
+dotenv.config();
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(`${process.env.REACT_APP_API_BASE_URL}/videos?part=snippet&chart=mostPopular&maxResults=25&key=${process.env.REACT_APP_API_SERVICE_KEY}`, requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  }, []);
+  return <VideoList videos={videos} />
 }
 
 export default App;
